7c096d80662f2b6465456a5b5a5cc60c41060cef
Juan Ignacio Toledo <ju.toledod@duocuc.cl>
2025-11-26 19:40:54 -0300

    añadir localhost:3000 a CORS

--- src/main/java/com/example/sneaker/config/CorsConfig.java
+++ src/main/java/com/example/sneaker/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("https://sneakerstore-chi.vercel.app") // o el dominio de tu frontend
+                        .allowedOrigins("https://sneakerstore-chi.vercel.app", "http://localhost:3000") // o el dominio de tu frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*");
             }
